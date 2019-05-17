c42b2a59dc6a53a1dbcbf95004d0b6a6f6ab6e47
infocuittesting <38007487+infocuittesting@users.noreply.github.com>
2019-05-17 13:42:49 +0530

    Update test_File.py

--- test_File.py
+++ test_File.py
@@ -1,5 +1,6 @@
+from sqlwrapper import *
 def test_Data(request):
-    d=request.json
-    gensql('insert','hotel_rooms',d)
+    d=  request.json
+    gensql('insert','test_Table',d)
     return("data_Inserted")
     
