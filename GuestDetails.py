c01004a1491e125077adefebdaceb44053f72c9c
infocuittesting <38007487+infocuittesting@users.noreply.github.com>
2019-05-22 16:07:21 +0530

    Update GuestDetails.py

--- GuestDetails.py
+++ GuestDetails.py
@@ -54,3 +54,23 @@ def Query_Guest_Details(request):
     
     return json.dumps({"Return": "Record Retrived Successfully","ReturnCode": "RRS",
                        "Returnvalue":gus_details},indent=2)
+def Query_Billing_Details(request):
+    d=request.json
+    guest = json.loads(dbget("select hotel_rooms.price,* from guest_details\
+	join hotel_rooms on hotel_rooms.room_no = guest_details.room_no\
+	 where guest_details.room_no='"+str(d['room_no'])+"' and checkout IS Null;"))
+    food= json.loads(dbget("select * from fb_requests where date(request_time) between '"+str(guest[0]['checkin_date'])+"' and '"+str(guest[0]['checkout_date'])+"' and room_no='"+str(d['room_no'])+"';"))
+    ldry = json.loads(dbget("	select * from ldry_request where date(request_time) between '"+str(guest[0]['checkin_date'])+"' and '"+str(guest[0]['checkout_date'])+"' and room_no='"+str(d['room_no'])+"';"))
+    total_amount_laundry = sum( dry['total_amount'] for dry in ldry)
+    print(total_amount_laundry)
+    total_amt_food = sum( fd['total_amount'] for fd in food)
+    print(total_amt_food)
+    d1 = datetime.strptime(guest[0]['checkin_date'], "%Y-%m-%d")
+    d2 = datetime.strptime(guest[0]['checkout_date'], "%Y-%m-%d")
+    total_days = abs((d2 - d1).days)
+    print(total_days)
+    total_price= total_days * guest[0]['price']
+    return json.dumps({"Return": "Record Retrived Successfully","ReturnCode": "RRS",
+                       "Food_beverage":food,"laundry":ldry,"room_price":guest,
+                       "total_amount":total_price+total_amount_laundry  +total_amt_food},indent=2)
+
