64f0e33052decf784e46cb249ee9398472e59751
infocuittesting <38007487+infocuittesting@users.noreply.github.com>
2019-05-30 20:41:38 +0530

    Update Query_front_desk_items.py

--- Query_front_desk_items.py
+++ Query_front_desk_items.py
@@ -8,7 +8,7 @@ def Select_Travel_Desk_Items(request):
     d = request.json
     output = json.loads(dbget("select fdcategory.fdcategory_id,fdcategory.fdcategory_name,fdcategory.fdcategory_image,frontdesk_items.fditem_id,\
                                frontdesk_items.fditem_names,frontdesk_items.fditem_image,\
-                               frontdesk_items.dept_id fdcategory,hotel_department.dept_name,hotel_department.dept_image from frontdesk_items \
+                               frontdesk_items.dept_id fdcategory,hotel_department.dept_name from frontdesk_items \
                                join  fdcategory on frontdesk_items.fdcategory_id=fdcategory.fdcategory_id \
                                join  hotel_department on frontdesk_items.dept_id=hotel_department.dept_id\
                                where frontdesk_items.business_id='"+str(d['business_id'])+"' and frontdesk_items.fdcategory_id= 'tra2095'"))
@@ -19,7 +19,7 @@ def Select_FrontDesk_Items(request):
     d = request.json
     output = json.loads(dbget("select fdcategory.fdcategory_id,fdcategory.fdcategory_name,fdcategory.fdcategory_image,frontdesk_items.fditem_id,\
                                frontdesk_items.fditem_names,frontdesk_items.fditem_image,\
-                               frontdesk_items.dept_id fdcategory,hotel_department.dept_name,hotel_department.dept_image from frontdesk_items \
+                               frontdesk_items.dept_id fdcategory,hotel_department.dept_name from frontdesk_items \
                                join  fdcategory on frontdesk_items.fdcategory_id=fdcategory.fdcategory_id \
                                join  hotel_department on frontdesk_items.dept_id=hotel_department.dept_id\
                                where frontdesk_items.business_id='"+str(d['business_id'])+"' and frontdesk_items.fdcategory_id= 'fro2219'"))
@@ -29,7 +29,7 @@ def Select_Extra_Items(request):
     d = request.json
     output = json.loads(dbget("select fdcategory.fdcategory_id,fdcategory.fdcategory_name,fdcategory.fdcategory_image,frontdesk_items.fditem_id,\
                                frontdesk_items.fditem_names,frontdesk_items.fditem_image,\
-                               frontdesk_items.dept_id fdcategory,hotel_department.dept_name,hotel_department.dept_image from frontdesk_items \
+                               frontdesk_items.dept_id fdcategory,hotel_department.dept_name from frontdesk_items \
                                join  fdcategory on frontdesk_items.fdcategory_id=fdcategory.fdcategory_id \
                                join  hotel_department on frontdesk_items.dept_id=hotel_department.dept_id\
                                where frontdesk_items.business_id='"+str(d['business_id'])+"' and frontdesk_items.fdcategory_id= 'ext1478'"))
