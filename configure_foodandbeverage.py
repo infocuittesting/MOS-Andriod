d631003ccd98c8121baee5dd84008b22269bd647
infocuittesting <38007487+infocuittesting@users.noreply.github.com>
2019-06-01 12:22:07 +0530

    add foodandbeverage_items for android

--- configure_foodandbeverage.py
+++ configure_foodandbeverage.py
@@ -96,6 +96,71 @@ def Update_Foodandbeverage_Items(request):
     c={ k : v for k,v in d.items() if k in('fbitem_id','business_id')}
     sql=gensql('update','foodandbeverage_items',b,c)
     return json.dumps({"Return": "Record Updated Successfully","ReturnCode": "RUS","Status": "Success","StatusCode": "200"},indent = 4)       
+def Display_Food_Menu_For_Android(request):
+    d = request.json
+    food_details,food_menu_details = [],[]
+    GET_FOOD_MENUS = json.loads(dbget("select foodtype.*,food_category.*,foodandbeverage_items.* from foodandbeverage_items\
+                                          join food_category on food_category.foodcateg_id = foodandbeverage_items.foodcategory_id \
+                                          join foodtype on foodtype.foodtype_id = foodandbeverage_items.foodtype_id\
+                                          where foodandbeverage_items.business_id = '"+str(d['business_id'])+"' "))
+       #get_best_sellers = json.loads(dbget(""))
+    for food_menu in GET_FOOD_MENUS:
+          if food_menu['foodcateg_name'] not in food_details:
+             food_details.append(food_menu['foodcateg_name'])
+             food_menu_details.append({"categry_name":food_menu['foodcateg_name'],"category_img":food_menu['foodcateg_image'],"items":[]})
+    for food_menu in GET_FOOD_MENUS:
+          for food_menu_detail in food_menu_details:
+            
+             if food_menu['foodcateg_name'] ==food_menu_detail['categry_name']:
+                food_menu['item_images'] = [{"item_image":food_menu['item_image']}]
+                food_menu_detail["items"].append(food_menu)
+    get_best_sellers= json.loads(dbget("select count(*),\
+                                       foodandbeverage_items.fbitem_id,foodandbeverage_items.item_name,\
+                                       foodandbeverage_items.item_image,foodandbeverage_items.price,foodandbeverage_items.foodtype_id,\
+                                       foodandbeverage_items.todayspecial_id,foodandbeverage_items.business_id,\
+                                       food_category.foodcateg_name,food_category.foodcateg_id,\
+                                       food_category.foodcateg_image,foodtype.foodtype_name,todayspecial.special from fb_collection\
+                                       join foodandbeverage_items on foodandbeverage_items.fbitem_id=fb_collection.fbitem_id\
+                                       join food_category on foodandbeverage_items.foodcategory_id=food_category.foodcateg_id\
+                                       join foodtype on foodandbeverage_items.foodtype_id=foodtype.foodtype_id\
+                                       join todayspecial on foodandbeverage_items.todayspecial_id=todayspecial.todayspecial_id\
+                                       where foodandbeverage_items.business_id='"+str(d['business_id'])+"'\
+                                       group by food_category.foodcateg_name,food_category.foodcateg_image,foodtype.foodtype_name,todayspecial.special,\
+                                       foodandbeverage_items.fbitem_id,foodandbeverage_items.item_name,food_category.foodcateg_id,\
+                                       foodandbeverage_items.item_image,foodandbeverage_items.price,foodandbeverage_items.foodtype_id,\
+                                       foodandbeverage_items.todayspecial_id,foodandbeverage_items.business_id\
+                                       order by count desc"))
+    k = [x['foodcateg_name'] for x in get_best_sellers]
+
+    new_vals=[]
+
+    for i in Counter(k):
+           all_values = [x for x in get_best_sellers if x['foodcateg_name']==i]
+           #all_values['']
+           #print("all",all_values)
+           new_vals.append(max(all_values, key=lambda x: x['count']))    
+    specials = json.loads(dbget("select food_category.foodcateg_id,food_category.foodcateg_name,food_category.foodcateg_image,foodandbeverage_items.*,\
+                                    todayspecial.special,\
+                                    foodtype.foodtype_id,foodtype.foodtype_name\
+                                    from foodandbeverage_items\
+                                    join todayspecial on foodandbeverage_items.todayspecial_id = todayspecial.todayspecial_id \
+                                    join food_category on food_category.foodcateg_id= foodandbeverage_items.foodcategory_id\
+                                    join foodtype on foodtype.foodtype_id = foodandbeverage_items.foodtype_id\
+                                    where foodandbeverage_items.todayspecial_id=1"))
+    
+    final_get_best_sellers_menu = [dict(item, item_images=[dict(item_image=item['item_image'])]) for item in new_vals]
+    today_specials = [dict(special, item_image=[dict(image_url=special['item_image'])]) for special in specials]
+    final_food_menu = [{"Food_Category":food_menu_details,
+                          "Best_Sellers":final_get_best_sellers_menu,
+                          "Today_Special":{"categry_name":"Today_Specials",
+                                           "category_img":"https://s3.amazonaws.com/image-upload-rekognition/tosfoodimages/new_work_cadillacmagazine-624x406.png",
+                                           "items":today_specials}}]
+
+    return json.dumps({"Return": "Record Retrived Successfully","ReturnCode": "RRS","Returnvalue":final_food_menu,"Status": "Success","StatusCode": "200"},indent = 4)
+
+
+
+
 
 
 
