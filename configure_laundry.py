e3ede964045d56c7082884556edf9ea5421b707d
infocuittesting <38007487+infocuittesting@users.noreply.github.com>
2019-06-11 15:02:47 +0530

    Update configure_laundry.py

--- configure_laundry.py
+++ configure_laundry.py
@@ -70,7 +70,7 @@ def Select_Laundry_Items(request):
    count = 0
    for item in finals:
 
-       count += 1
+       count += 0
        item.update({'count':str(count),'category_image':''})
        print(count)
 
